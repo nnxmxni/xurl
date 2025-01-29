e704836e802f547d16f3cafca3fe918882dd09e3
Joshua Nnamani <joshchief169@gmail.com>
2025-01-29 02:49:32 +0100

    feat: Set request body size limit to 1mb
    - Add limit of 1,048,576 bytes (1mb) to request body size.
    - Use `http.MaxBytesReader` to enforce limit.
    - This helps prevent the client from sending oversized payloads.

--- util/util.go
+++ util/util.go
@@ -3,17 +3,25 @@ package util
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/go-playground/validator/v10"
+	"math/rand"
 	"net/http"
 )
 
+var Validate = validator.New()
+
 type APIResponseBody struct {
 	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
 }
 
 func ParseJSON(w http.ResponseWriter, r *http.Request, payload interface{}) error {
 	if r.Body == nil {
 		return fmt.Errorf("request body is empty")
 	}
+
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
@@ -25,14 +33,14 @@ func ParseJSON(w http.ResponseWriter, r *http.Request, payload interface{}) erro
 	return nil
 }
 
-func WriteJSON(w http.ResponseWriter, r *http.Request, status int, response APIResponseBody) error {
+func WriteJSON(w http.ResponseWriter, status int, response APIResponseBody) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
 	return json.NewEncoder(w).Encode(response)
 }
 
-func WriteError(w http.ResponseWriter, r *http.Request, status int, err error) error {
+func WriteError(w http.ResponseWriter, status int, err error) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
@@ -41,4 +49,15 @@ func WriteError(w http.ResponseWriter, r *http.Request, status int, err error) e
 	}
 
 	return json.NewEncoder(w).Encode(response)
+}
+
+func RandStringRunes(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, length)
+
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(b)
 }
