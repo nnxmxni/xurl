df3b0e5432ef0eea8f8e95ec73c03c746f3045ed
Joshua Nnamani <joshchief169@gmail.com>
2025-01-29 03:46:08 +0100

    feat: Add store interface with Create and Get methods
    - Add ErrURLNotFound error variable to store package

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,5 +1,13 @@
 package store
 
+import (
+	"context"
+	"errors"
+)
+
+var ErrURLNotFound = errors.New("resource not found")
+
 type Store interface {
-	Create(string, string) (string, error)
+	Create(context.Context, URLMapper) (string, error)
+	Get(context.Context, URLMapper) (string, error)
 }
